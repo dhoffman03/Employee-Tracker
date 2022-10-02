8221d2ba7b2f78ad944e79cbc095f507afc73ddf
Dymond <107832519+dhoffman03@users.noreply.github.com>
2022-10-01 23:54:38 -0700

    Constraint added and foriegn_key_checks set to 0

--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,8 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
+SET FOREIGN_KEY_CHECKS = 0;
+
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
@@ -12,7 +14,7 @@ CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
+    department_id INT,
     FOREIGN KEY (department_id)
         REFERENCES department(id)
         ON DELETE CASCADE
@@ -23,13 +25,8 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30), 
     last_name VARCHAR(30),
-    role_id INT NOT NULL,
-    manager_id INT NULL,
-    FOREIGN KEY (role_id)
-        REFERENCES role(id)
-        ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (manager_id)
-        REFERENCES employee(id)
-        ON DELETE SET NULL
-        ON UPDATE CASCADE
+    role_id INT,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    manager_id INT ,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
