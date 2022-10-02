5d65f97f3e6f355ae20b600730dfaf091982f16d
Dymond <dymond.hoffman03@gmail.com>
2022-10-01 20:16:57 -0700

    schema set up

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,35 @@
-DROP DATABASE IF EXISTS employee_tracker_db;
-CREATE DATABASE employee_tracker_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
-USE employee_tracker_db;
+USE employee_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    FOREIGN KEY (department_id)
+        REFERENCES department(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
-    first_name VARCHAR(30),
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30), 
     last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
-    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    FOREIGN KEY (role_id)
+        REFERENCES role(id)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (manager_id)
+        REFERENCES employee(id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
 );
